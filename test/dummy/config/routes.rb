e8ab70668b32708f3c82ad69e7064da9adece5cf
a-chacon <andres.ch@protonmail.com>
2024-07-31 09:32:40 -0400

    fix(route_extractor): Verify the correct implementation of a route. If the controller or method don't exist, then discard the route.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/users/login', to: 'users#login'
+  get "/users/new", to: "users#new" # This route is for testing purpose
   resources :users, shallow: true do
     resources :projects
   end
