2991a481c869ae7afd2e01725aa36812c2ee6931
a-chacon <andres.ch@protonmail.com>
2024-08-13 15:42:54 -0400

    fix: schema version

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_211538) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_18_211538) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
