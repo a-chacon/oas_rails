4c79c9126f8c9278c382a554a07fe0a610cd9318
a-chacon <andres.ch@protonmail.com>
2024-08-13 17:07:20 -0400

    fix: bundle rubocop

--- lib/oas_rails/spec/specable.rb
+++ lib/oas_rails/spec/specable.rb
@@ -35,9 +35,11 @@ module OasRails
         hash
       end
 
+      # rubocop:disable Lint/UnusedMethodArgument
       def as_json(options = nil)
         to_spec
       end
+      # rubocop:enable Lint/UnusedMethodArgument
 
       private
 
