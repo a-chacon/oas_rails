aedbe7b610c3f5db5cadf8e59495351f8e83eddf
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-08-30 18:52:30 +0200

    test: feature ignore custom route (#55)
    * Ignore custom routes
    
    * adding docs
    
    * fix lint
    
    * Fix rubocop
    
    * Editing method ignoring action + test
    
    * Last fix initializer
    
    * Fix changelog ? edit test route extractor
    
    * last fix. Sorry for that...
    
    * Fix test.

--- test/lib/oas_rails/extractors/route_extractor_test.rb
+++ test/lib/oas_rails/extractors/route_extractor_test.rb
@@ -2,6 +2,7 @@ module OasRails
   module Builders
     class RouteExtractorTest < Minitest::Test
       def test_without_custom_routes
+        Extractors::RouteExtractor.clear_cache
         OasRails.config.ignored_actions = []
         result = Extractors::RouteExtractor.host_routes
         assert_equal 14, result.count
@@ -9,17 +10,25 @@ module OasRails
       end
 
       def test_with_custom_controllers_actions
-        OasRails.config.ignored_actions = ["projects#index"]
-        result = Extractors::RouteExtractor.host_routes
-        assert_equal 13, result.count
         Extractors::RouteExtractor.clear_cache
+        OasRails.config.ignored_actions = []
+        init_count = Extractors::RouteExtractor.host_routes.count
+        OasRails.config.ignored_actions = ["projects#index"]
+        Extractors::RouteExtractor.clear_cache
+        result = Extractors::RouteExtractor.host_routes.count
+        assert_equal (init_count - 1), result
+        Extractors::RouteExtractor.clear_cache
+        OasRails.config.ignored_actions = []
       end
 
       def test_with_custom_controller_only
-        OasRails.config.ignored_actions = ["projects"]
-        result = Extractors::RouteExtractor.host_routes
-        assert_equal 8, result.count
         Extractors::RouteExtractor.clear_cache
+        OasRails.config.ignored_actions = ["projects"]
+        routes = Extractors::RouteExtractor.host_routes
+        result = routes.select { |route| route.controller.include?("projects") }
+        assert_equal 0, result.count
+        Extractors::RouteExtractor.clear_cache
+        OasRails.config.ignored_actions = []
       end
     end
   end
