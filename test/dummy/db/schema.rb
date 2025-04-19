36cb39bca1d377782da39439f92468d1d78fae04
Andrés <39093711+a-chacon@users.noreply.github.com>
2025-04-19 19:19:05 +0200

    fix: update easy_tal to easy_talk_two fixing conversions of boolean and decimal fields (#113)

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_18_211538) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_19_163914) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: false
+    t.date "start_date"
+    t.datetime "end_date"
+    t.decimal "budget", precision: 10, scale: 2
+    t.integer "priority"
+    t.string "status", default: "draft"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
