57d33f379fa7197f59fcc5fe46032b80ffbf1255
Jack <5182053+phyzical@users.noreply.github.com>
2025-08-05 14:48:24 +0800

    missed ,
    Signed-off-by: Jack <5182053+phyzical@users.noreply.github.com>

--- app/helpers/oas_rails/oas_rails_helper.rb
+++ app/helpers/oas_rails/oas_rails_helper.rb
@@ -2,7 +2,7 @@ module OasRails
   module OasRailsHelper
     def rapi_doc_config
       {
-        "spec-url" => "#{OasRails::Engine.routes.find_script_name({})}.json"
+        "spec-url" => "#{OasRails::Engine.routes.find_script_name({})}.json",
         "show-header" => "true",
         "font-size" => "largest",
         "show-method-in-nav-bar" => "as-colored-text",
