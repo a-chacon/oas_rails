71d46ceec51c2cd5abb5bb3a375d086de8cdb7f4
Jack <5182053+phyzical@users.noreply.github.com>
2025-08-05 14:15:29 +0800

    Add initial view helper for rapi-doc-configs
    Signed-off-by: Jack <5182053+phyzical@users.noreply.github.com>

--- app/helpers/oas_rails/oas_rails_helper.rb
+++ app/helpers/oas_rails/oas_rails_helper.rb
@@ -1,4 +1,21 @@
 module OasRails
   module OasRailsHelper
+    def rapi_doc_config
+      {
+        "spec-url" => "#{OasRails::Engine.routes.find_script_name({})}.json"
+        "show-header" => "true",
+        "font-size" => "largest",
+        "show-method-in-nav-bar" => "as-colored-text",
+        "nav-item-spacing" => "relaxed",
+        "allow-spec-file-download" => "true",
+        "schema-style" => "table",
+        "sort-tags" => "true",
+        "persist-auth" => "true"
+      }
+    end
+
+    def logo_url
+      false
+    end
   end
 end
